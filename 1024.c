f329dc82d295ed05fa5ca1ecf6a4f66720e162fc
sujangJin <sujang1@naver.com>
2016-09-30 18:54:22 +0900

    baekjoon judge 1024

--- 1024.c
+++ 1024.c
@@ -14,12 +14,14 @@ int main()
 		return;
 	}
 	while (input_cnt <= 100){
+		if (input_cnt % 2 == 0) // Even number
 		{
 			res_start = (input_sum / input_cnt) - (input_cnt / 2 - 1);
 			tmp_sum = res_start * input_cnt + (input_cnt * (input_cnt - 1) / 2);
 			if (res_start < 0 || tmp_sum != input_sum)	input_cnt++;
 			else break;
 		}
+		else			// Odd number
 		{
 			res_start = (input_sum / input_cnt) - (input_cnt / 2);
 			tmp_sum = res_start * input_cnt + (input_cnt * (input_cnt - 1) / 2);
