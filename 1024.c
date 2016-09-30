945945bba6dc129dfa75d8d0ae90150ccd57d8f2
sujangJin <sujang1@naver.com>
2016-09-30 18:53:57 +0900

    backjoon judge 1024

---
+++ 1024.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+
+int main()
+{
+	int input_sum = 0, input_cnt = 0;
+
+	int res_start, i, tmp_sum;
+	
+	scanf("%d %d", &input_sum, &input_cnt);
+
+	if (input_cnt < 2 || input_sum > 1000000000 || input_cnt > 100)
+	{
+		printf("-1 \n");
+		return;
+	}
+	while (input_cnt <= 100){
+		{
+			res_start = (input_sum / input_cnt) - (input_cnt / 2 - 1);
+			tmp_sum = res_start * input_cnt + (input_cnt * (input_cnt - 1) / 2);
+			if (res_start < 0 || tmp_sum != input_sum)	input_cnt++;
+			else break;
+		}
+		{
+			res_start = (input_sum / input_cnt) - (input_cnt / 2);
+			tmp_sum = res_start * input_cnt + (input_cnt * (input_cnt - 1) / 2);
+			if (res_start < 0 || tmp_sum != input_sum)	input_cnt++;
+			else break;
+		}
+	}
+	
+	if (input_cnt > 100)
+	{
+		printf("-1 \n");
+	}
+	else {
+		for (i = 0; i < input_cnt; i++)
+		{
+			printf("%d ", res_start++);
+		}
+	}
+	return 0;
+}
