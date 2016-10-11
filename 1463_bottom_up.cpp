d77d644f118233e1415a881c9dbd7530ff5a50a5
sujangJin <sujang1@naver.com>
2016-10-11 20:32:39 +0900

    dynamic programming

---
+++ 1463_bottom_up.cpp
@@ -0,0 +1,27 @@
+// this code was written following URL
+// URL : http://blog.naver.com/PostView.nhn?blogId=kks227&logNo=220777103650&parentCategoryNo=&categoryNo=299&viewDate=&isShowPopularPosts=false&from=postList
+// 'BOTTOM-DOWN' method of Dynamic Programming 
+
+#include<cstdio>
+#include<algorithm>
+using namespace std;
+
+const int MAX = 1000001;
+
+int main(){
+	int N, dp[MAX];
+	scanf("%d", &N);
+
+	fill(dp, dp + MAX, 1000000000);
+	dp[1] = 0; // base case
+	for (int i = 1; i < N; i++){
+		// use minimal function to choose 
+		// whether to divide or not(just remove 1, -1)
+		dp[i + 1] = min(dp[i + 1], dp[i] + 1);
+		if (i * 2 < MAX) dp[i * 2] = min(dp[i * 2], dp[i] + 1);	
+		if (i * 3 < MAX) dp[i * 3] = min(dp[i * 3], dp[i] + 1);
+	}
+
+	printf("%d", dp[N]);
+	return 0;
+}
---
+++ 1463_top_down.cpp
@@ -0,0 +1,29 @@
+// this code was written following URL
+// URL : http://blog.naver.com/PostView.nhn?blogId=kks227&logNo=220777103650&parentCategoryNo=&categoryNo=299&viewDate=&isShowPopularPosts=false&from=postList
+// 'TOP-DOWN' method of Dynamic Programming 
+
+#include<cstdio>
+#include<algorithm>
+
+using namespace std;
+const int MAX = 1000001;
+
+int dp[MAX];
+
+int f(int n){
+	if (n == 1) return 0;
+	if (dp[n] != -1) return dp[n];
+
+	int result = f(n - 1) + 1;
+	if (n % 3 == 0) result = min(result, f(n / 3) + 1);
+	if (n % 2 == 0) result = min(result, f(n / 2) + 1);
+	dp[n] = result;
+	return result;
+}
+
+int main(){
+	int N;
+	scanf("%d",&N);
+	fill(dp, dp + MAX, -1);
+	printf("%d\n", f(N));
+}
